433bee837853858d6f36c7ad080558e7da37cdc8
frankw <fwessels@xs4all.nl>
2017-05-25 13:46:09 -0700

    Add dual timeout increase

--- dynamic-timeouts_test.go
+++ dynamic-timeouts_test.go
@@ -21,3 +21,25 @@ func TestSingleIncreaseTimeout(t *testing.T) {
 	adjusted := timeout.Timeout()
 	fmt.Println(adjusted)
 }
+
+func TestDualIncreaseTimeout(t *testing.T) {
+
+	timeout := NewDynamicTimeout(time.Minute)
+
+	initial := timeout.Timeout()
+	fmt.Println(initial)
+
+	for i := 0; i < DynTimeOutLogSize; i++ {
+		timeout.LogFailure()
+	}
+
+	adjusted := timeout.Timeout()
+	fmt.Println(adjusted)
+
+	for i := 0; i < DynTimeOutLogSize; i++ {
+		timeout.LogFailure()
+	}
+
+	adjustedAgain := timeout.Timeout()
+	fmt.Println(adjustedAgain)
+}
