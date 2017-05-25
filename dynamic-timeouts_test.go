2e35a8519341b48868bdfa0087ace85f05238b08
frankw <fwessels@xs4all.nl>
2017-05-25 13:46:34 -0700

    Add single and dual timeout decrease

--- dynamic-timeouts_test.go
+++ dynamic-timeouts_test.go
@@ -43,3 +43,40 @@ func TestDualIncreaseTimeout(t *testing.T) {
 	adjustedAgain := timeout.Timeout()
 	fmt.Println(adjustedAgain)
 }
+
+func TestSingleDecreaseTimeout(t *testing.T) {
+
+	timeout := NewDynamicTimeout(time.Minute)
+
+	initial := timeout.Timeout()
+	fmt.Println(initial)
+
+	for i := 0; i < DynTimeOutLogSize; i++ {
+		timeout.LogSuccess(20 * time.Second)
+	}
+
+	adjusted := timeout.Timeout()
+	fmt.Println(adjusted)
+}
+
+func TestDualDecreaseTimeout(t *testing.T) {
+
+	timeout := NewDynamicTimeout(time.Minute)
+
+	initial := timeout.Timeout()
+	fmt.Println(initial)
+
+	for i := 0; i < DynTimeOutLogSize; i++ {
+		timeout.LogSuccess(20 * time.Second)
+	}
+
+	adjusted := timeout.Timeout()
+	fmt.Println(adjusted)
+
+	for i := 0; i < DynTimeOutLogSize; i++ {
+		timeout.LogSuccess(20 * time.Second)
+	}
+
+	adjustedAgain := timeout.Timeout()
+	fmt.Println(adjustedAgain)
+}
